f0237ec6ddc2801d10111e4df72b7d780fe88cbc
Mohammed-Adnan-04 <adnangulfarosh098@gmail.com>
2024-07-19 23:25:59 +0530

    created rock.papper,sissor game

--- new.py
+++ new.py
@@ -1,2 +1,12 @@
-for i in range(10):
-    print(i*"*")
+import random
+choices = ["rock","papper","sissor"]
+computer_choice = random.choice(choices)
+user_choice = str(input("enter your choice: "))
+if user_choice == computer_choice:
+    print("its draw")
+elif user_choice=="rock" and computer_choice=="sissor":
+    print(f"you win {user_choice} beats {computer_choice}")
+elif user_choice=="sissor" and computer_choice=="rock":
+    print(f"you lose {computer_choice} beats {user_choice}")
+elif user_choice=="papper" and computer_choice == "rock":
+    print(f"you win {user_choice} beats {computer_choice}")
